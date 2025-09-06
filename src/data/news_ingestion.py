d8d8422b85c3183c4dfc990d71766e50fd51b008
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-09-06 22:31:57 +0700

    fix : batch fetching news api 15 days/batch for over 90 days script

--- src/data/news_ingestion.py
+++ src/data/news_ingestion.py
@@ -1,43 +1,80 @@
+import os
 import requests
 import pandas as pd
-import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # for accessing environment variables
+from datetime import datetime, timedelta
 
 load_dotenv()
 
 # API Key Polygon.io
-POLYGON_API_KEY = os.getenv("POLYGON_API_KEY") 
+POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+if not POLYGON_API_KEY:
+    raise ValueError("POLYGON_API_KEY not found in environment variables")
+
+# Chunking configuration
 TICKER = "AAPL"
+TOTAL_DAYS = 105
+CHUNK_DAYS = 15
 
-# Rentang waktu
-start_date = "2025-08-01"  # YYYY-MM-DD
-end_date = "2025-08-30"
+all_articles = []
+print(f'Starting to fetch {TICKER} news articles for {TOTAL_DAYS} days')
 
-# Endpoint untuk berita ticker dengan rentang waktu
-url = (
-    f"https://api.polygon.io/v2/reference/news?"
-    f"ticker={TICKER}&limit=100&apiKey={POLYGON_API_KEY}"
-    f"&published_utc.gte={start_date}&published_utc.lte={end_date}"
-)
+# Loop fetch data for 105 days from now with 15 days chunk
+for i in range(0, TOTAL_DAYS, CHUNK_DAYS):
+    # Start day and end day for each chunk
+    end_date = datetime.now() - timedelta(days=i)
+    start_date = end_date - timedelta(days=CHUNK_DAYS)
 
-response = requests.get(url)
-data = response.json()
+    start_date_str = start_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
 
-if response.status_code == 200 and data.get("results"):
-    df = pd.DataFrame(data["results"])
+    print(f'Fetching news for {TICKER} from {start_date} to {end_date}')
+
+    # Endpoint for polygon api fetching
+    url = (
+        f"https://api.polygon.io/v2/reference/news?ticker={TICKER}"
+        f"&published_utc.gte={start_date_str}&published_utc.lte={end_date_str}"
+        f"&limit=100&apiKey={POLYGON_API_KEY}"
+    )
+
+    # Pegination, if more than 100 articles in 15 days
+    while url:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            articles_in_page = data.get('result', [])
+            if not articles_in_page:
+                print('There is no article more in this page')
+                break
+
+            all_articles.extend(articles_in_page)
+            print(f'Found {len(articles_in_page)} articles in this page, Total Now : {len(all_articles)}')
+
+            # Next url for pagination
+            url = data.get('next_url')
+            if url:
+                url += f'&apiKey={POLYGON_API_KEY}'
+
+            # Limit polygon (5 request per minutes)
+            time.sleep(15)
+
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            url = None
+
+# Create dataframe from data parsed
+if all_articles:
+    df = pd.DataFrame(all_articles)
     df = df[['published_utc', 'title', 'description', 'article_url']]
     df.rename(columns={'published_utc': 'publishedAt'}, inplace=True)
     df['publishedAt'] = pd.to_datetime(df['publishedAt']).dt.tz_localize(None)
 
-    # Pastikan folder data/raw ada
-    os.makedirs("data/raw", exist_ok=True)
-    csv_path = f"data/raw/{TICKER}_news_data.csv"
-    df.to_csv(csv_path, index=False)
-    
-    print(f"Successfully fetched {len(df)} news articles for {TICKER}")
-    print(f"Saved to {csv_path}")
-    print()
-    print(df.head())
-    
+    # Save to csv
+    DATA_PATH = 'data/raw/AAPL_news_data.csv'
+    os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
+    df.to_csv(DATA_PATH, index=False)
+    print(f'News articles for {TICKER} saved to {DATA_PATH}')
 else:
-    print(f"Failed to fetch news: {data}")
+    print(f'No articles fetched for {TICKER}')
